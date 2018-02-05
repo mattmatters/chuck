9aea11c3e22b40186744d91923d01dc08ed17dbf
Matt Lewis <domattthings@gmail.com>
2018-02-05 18:22:16 -0500

    First commit

---
+++ app/src/main/java/io/mattmatters/pocket_chuck/chucktivity.kt
@@ -0,0 +1,12 @@
+package io.mattmatters.pocket_chuck
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class chucktivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chucktivity)
+    }
+}
