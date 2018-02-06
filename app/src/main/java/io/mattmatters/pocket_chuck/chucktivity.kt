4875cd1e07fd4310dc3b62f1d6a8a55a26a57327
Matt Lewis <domattthings@gmail.com>
2018-02-05 19:27:55 -0500

    Intent activity

--- app/src/main/java/io/mattmatters/pocket_chuck/chucktivity.kt
+++ app/src/main/java/io/mattmatters/pocket_chuck/chucktivity.kt
@@ -1,12 +1,22 @@
 package io.mattmatters.pocket_chuck
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+
+const val EXTRA_MESSAGE = "io.mattmatters.pocket_chuck.MESSAGE"
 
 class chucktivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chucktivity)
+}
+
+    /** Called when the user taps the Send button */
+    fun sendMessage(view: View) {
+        val intent = Intent(this, DisplayMessageActivity::class.java)
+        startActivity(intent)
     }
 }
